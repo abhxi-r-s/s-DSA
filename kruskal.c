a2a28b5515ef9c2116d3f0018a83f6b984865993
abhxi-r-s <abhishekunni8281@gmail.com>
2025-01-12 02:56:22 +0530

    .

--- kruskal.c
+++ kruskal.c
@@ -1,82 +1,102 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<limits.h>
+int n,a[20][20],parent[20],rank[20],mst[20][20],numEdge=0;
+struct edge
+{
+int v;
+int u;
+int cost;
+};
+struct edge *getminEdge()
+{
+    struct edge *Edge=(struct edge*)malloc(sizeof(struct edge));
+    int min=999,minweight=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(a[i][j]!=0 && a[i][j]<min)
+            {
+                minweight=a[i][j];
+                Edge->u=i;
+                Edge->v=j;
+                Edge->cost=a[i][j];
+            }
+        }
+    }
+    a[Edge->u][Edge->v]=INT_MAX;
+    return Edge;
 
-#define MAX 100
-
-typedef struct {
-    int u, v, weight;
-} Edge;
-
-Edge edges[MAX];  // List of all edges
-int parent[MAX];  // Union-Find parent array
-
-// Function to find the root of a node
-int find(int i) {
-    while (parent[i] != i)
-        i = parent[i];
-    return i;
+}
+int find(int node)
+{
+    if(parent[node]==node)
+    {
+        return node;
+    }
+    else
+    {
+        return find(parent[node]);
+    }
+}
+void setUnion(int u,int v)
+{
+    if(rank[u]>rank[v])
+    {
+        parent[v]=u;
+    }
+    else if(rank[v]>rank[u])
+    {
+        parent[u]=v;
+    }
+    else
+    {
+        parent[v]=u;
+        rank[u]+=1;
+    }
 }
 
-// Function to union two subsets
-void unionSets(int u, int v) {
-    int rootU = find(u);
-    int rootV = find(v);
-    parent[rootU] = rootV;
-}
+void main()
+{
+    printf("Enter the number of nodes :");
+    scanf("%d",&n);
 
-// Comparison function for sorting edges by weight
-int compare(const void *a, const void *b) {
-    Edge *edgeA = (Edge *)a;
-    Edge *edgeB = (Edge *)b;
-    return edgeA->weight - edgeB->weight;
-}
+    printf("Ente the adjacency matrix :");
+    for(int i=0;i<n;i++)
+    {
+        parent[i]=i;
+        rank[i]=0;
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+            mst[i][j]=0;
+        }
+    }
+    while(numEdge<n-1)
+    {
+        struct edge *minEdge;
+        minEdge=getminEdge();
+        int u=minEdge->u;
+        int v=minEdge->v;
 
-void kruskal(int n, int e) {
-    int mstCost = 0, mstEdges = 0;
-
-    // Initialize parent array for union-find
-    for (int i = 0; i < n; i++)
-        parent[i] = i;
-
-    // Sort edges by weight
-    qsort(edges, e, sizeof(Edge), compare);
-
-    printf("Edges in the Minimum Spanning Tree:\n");
-    for (int i = 0; i < e; i++) {
-        int u = edges[i].u;
-        int v = edges[i].v;
-        int weight = edges[i].weight;
-
-        // Check if adding this edge creates a cycle
-        if (find(u) != find(v)) {
-            printf("{%d, %d} = %d\n", u, v, weight);
-            unionSets(u, v);  // Merge the two sets
-            mstCost += weight;
-            mstEdges++;
-
-            // Stop if MST is complete (n-1 edges)
-            if (mstEdges == n - 1)
-                break;
+        if(find(u)!=find(v))
+        {
+            setUnion(u,v);
+            mst[u][v]=minEdge->cost;
+            numEdge++;
+        }
+    }
+    printf("MST nodes and edges :\n");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(mst[i][j]!=0)
+            {
+                printf("\n %d-%d is %d",i,j,mst[i][j]);
+            }
         }
     }
 
-    printf("Total cost of the Minimum Spanning Tree: %d\n", mstCost);
-}
-
-int main() {
-    int n, e;
-
-    printf("Enter the number of vertices: ");
-    scanf("%d", &n);
-    printf("Enter the number of edges: ");
-    scanf("%d", &e);
-
-    printf("Enter the edges (u, v, weight):\n");
-    for (int i = 0; i < e; i++) {
-        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].weight);
-    }
-
-    kruskal(n, e);
-
-    return 0;
+    
 }
